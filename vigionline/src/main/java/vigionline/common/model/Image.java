7899ebfb416ac0ef91b59e1e21ba37afc27bc7e2
unknown <rafael@rafael-win7.(none)>
2012-03-27 21:07:29 +0100

    Implemented Image and ImageMapper

---
+++ vigionline/src/main/java/vigionline/common/model/Image.java
@@ -0,0 +1,24 @@
+package vigionline.common.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Image implements Serializable {
+
+	private static final long serialVersionUID = -7366477520675577432L;
+	private int _idImage, _size;
+	private Date _date;
+	private String _filename;
+	
+	public int getIdImage() { return _idImage;	}
+	public void setIdImage(int idImage) { this._idImage = idImage; }
+	public int getSize() { return _size; }
+	public void setSize(int size) {	this._size = size;	}
+	public Date getDate() {	return _date; }
+	public void setDate(Date date) { this._date = date; }
+	public String getFilename() { return _filename; }
+	public void setFilename(String filename) { this._filename = filename; }
+}

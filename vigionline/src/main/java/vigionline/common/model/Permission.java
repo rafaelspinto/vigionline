c84a123e65cf2fec832e5bef7389a886a16ff052
Rafael Pinto <santospinto.rafael@gmail.com>
2012-03-27 15:28:28 +0100

    Implemented PermissionTypeMapper and testPermissionTypeCRUD

--- vigionline/src/main/java/vigionline/common/model/Permission.java
+++ vigionline/src/main/java/vigionline/common/model/Permission.java
@@ -7,10 +7,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Permission implements Serializable {
 
 	private static final long serialVersionUID = -692473828444121530L;
-	private int _idPermission, _idPermissionType;
+	private int _idPermission, _idPermissionType, _idCamera;
 
 	public int getIdPermission() {	return _idPermission; }
 	public void setIdPermission(int idPermission) {	this._idPermission = idPermission; }
 	public int getIdPermissionType() {	return _idPermissionType; }
 	public void setIdPermissionType(int idPermissionType) {	this._idPermissionType = idPermissionType; }
+	public int getIdCamera() { return _idCamera; }
+	public void setIdCamera(int idCamera) { this._idCamera = idCamera; }
 }

9135b6989374cb3f6bf8578325d79ddf860a2bd0
Rafael Pinto <santospinto.rafael@gmail.com>
2012-03-28 15:57:00 +0100

    Implementing LocationsController (VWC)

--- vigionline/src/main/java/vigionline/vwc/controller/LocationsController.java
+++ vigionline/src/main/java/vigionline/vwc/controller/LocationsController.java
@@ -2,15 +2,18 @@ package vigionline.vwc.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.view.Viewable;
 
-@Path("/locations")
+@Path("locations")
 public class LocationsController {
 
 	@GET
+	@Produces(MediaType.TEXT_HTML)
 	public Viewable getLocations()
 	{
-		return new Viewable("vigionline/vwc/view/locations.jsp");
+		return new Viewable("/locations.jsp",this);
 	}
 }

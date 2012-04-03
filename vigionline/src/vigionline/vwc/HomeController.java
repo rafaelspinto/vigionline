e76b3e1b346f76da3eb8fa14d650493866f749e5
unknown <rafael@rafael-win7.(none)>
2012-04-03 22:40:43 +0100

    Implemented VWC language chooser. Starting to define general layout.

--- vigionline/src/vigionline/vwc/HomeController.java
+++ vigionline/src/vigionline/vwc/HomeController.java
@@ -2,6 +2,10 @@ package vigionline.vwc;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 import com.sun.jersey.api.view.Viewable;
 
@@ -12,5 +16,15 @@ public class HomeController {
 	public Viewable index()
 	{
 		return new Viewable("/index");
+	}
+	
+	@GET
+	@Path("setLang")
+	public Response setLang(@QueryParam("country") String country,
+			@QueryParam("lang") String lang)
+	{
+		return Response.seeOther(UriBuilder.fromUri("/").build()).
+				cookie(new NewCookie[] {new NewCookie("lang", lang),new NewCookie("country", country)}).
+				build();
 	}
 }

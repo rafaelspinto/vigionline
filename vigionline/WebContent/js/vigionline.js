6e35a91ea1f23e9bfc5ff10f723c0fcc540b82ab
Rafael Pinto <santospinto.rafael@gmail.com>
2012-04-24 14:38:32 +0100

    Created ValidateForm in vigionline.js and used it in create_user.jsp

--- vigionline/WebContent/js/vigionline.js
+++ vigionline/WebContent/js/vigionline.js
@@ -6,3 +6,22 @@ function AppendToMenu(menu_id, url, name)
 		            $('<span>').attr('class', 'tab').append(name)
 		))); 
 }
+
+function ValidateForm(form_id, fld, v_fld)
+{
+	$("#"+form_id).submit(function(e)
+			{
+				var pwd = $("#"+fld).val();
+				var cpwd = 	$("#"+v_fld).val();
+				if( pwd != cpwd)
+				{
+					e.preventDefault();
+					$("#"+v_fld).attr('class','error');
+					var div = $("#cpwd").parents("div.control-group");
+					div.removeClass("success");
+					div.addClass("error");
+				}
+				else
+					this.submit();
+			});
+}
